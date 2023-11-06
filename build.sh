f05238db8020aa6dd28880446b18c9bdccc09610
B33Boy <abhip202@gmail.com>
2023-11-05 23:23:22 -0500

    updated build script

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-mkdir 01_basic_server_client/build
+mkdir -p 01_basic_server_client/build
 g++ -o 01_basic_server_client/build/myclient $(pwd)/01_basic_server_client/myclient.cpp
 g++ -o 01_basic_server_client/build/myserver $(pwd)/01_basic_server_client/myserver.cpp
 
+
+mkdir -p 02_protocol_parsing/build
+g++ -o 02_protocol_parsing/build/myclient $(pwd)/02_protocol_parsing/myclient.cpp
+g++ -o 02_protocol_parsing/build/myserver $(pwd)/02_protocol_parsing/myserver.cpp
