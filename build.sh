eaf49c469b3163e4b79160044ece8b3ebf1e6fb4
B33Boy <abhip202@gmail.com>
2025-01-31 19:26:25 -0500

    move old event_loop into archive

--- build.sh
+++ build.sh
@@ -7,3 +7,7 @@ g++ -o 01_basic_server_client/build/myserver $(pwd)/01_basic_server_client/myser
 mkdir -p 02_protocol_parsing/build
 g++ -o 02_protocol_parsing/build/myclient $(pwd)/02_protocol_parsing/myclient.cpp
 g++ -o 02_protocol_parsing/build/myserver $(pwd)/02_protocol_parsing/myserver.cpp
+
+mkdir -p 03_event_loop/build
+g++ -o 03_event_loop/archive/build/myclient $(pwd)/03_event_loop/archive/myclient.cpp
+g++ -o 03_event_loop/archive/build/myserver $(pwd)/03_event_loop/archive/myserver.cpp
